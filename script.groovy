24a4de83dc51379841179a02089775675860f9ee
AbdelrahmanElshahat <abdelrahmanelshahat00@gmail.com>
2025-05-26 18:22:17 +0300

    firstcommit

---
+++ script.groovy
@@ -0,0 +1,12 @@
+def buildApp(){
+    echo 'Building the application...'
+    sh 'mvn clean package -DskipTests'
+}
+def buildImage(){
+    echo 'Building the docker image...'
+    withCredentials([usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
+        sh "docker build -t elshahat20/my-app:1.0 ."
+        sh "echo $$DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+        sh "docker push elshahat20/my-app:1.0"
+    }
+}
