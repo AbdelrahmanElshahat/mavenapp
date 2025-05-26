7e824818cb08eb46d258e9d12e5cf14845971457
AbdelrahmanElshahat <abdelrahmanelshahat00@gmail.com>
2025-05-26 18:27:08 +0300

    editfile

--- script.groovy
+++ script.groovy
@@ -6,7 +6,7 @@ def buildImage(){
     echo 'Building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'docker-repo', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh "docker build -t elshahat20/my-app:1.0 ."
-        sh "echo $$DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
+        sh "echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin"
         sh "docker push elshahat20/my-app:1.0"
     }
 }
